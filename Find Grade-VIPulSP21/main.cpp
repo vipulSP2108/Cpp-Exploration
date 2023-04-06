d502d9174ed68fb0936f17e2a77014e23e56f358
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-04-06 06:16:56 +0530

    Create main.cpp

---
+++ Find Grade-VIPulSP21/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int marks;
+  cin>>marks;
+  if (marks>=90){
+    cout<<"A";
+  }
+  else if (marks>=80){
+    cout<<"B";
+  }
+  else if (marks>=70){
+    cout<<"C";
+  }
+  else{
+    cout<<"D";
+  }
+}
