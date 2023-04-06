8aed16debf120b17d5705793f8606f1457e9f5aa
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-04-06 06:06:58 +0530

    Create main.cpp

---
+++ Divisible by n-VIPulSP/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+  int num1,num2;
+  cin>>num1;
+  cin>>num2;
+  if (num1%num2==0){
+    cout<<"YES";
+  }
+  else{
+    cout<<"NO";
+  }
+}
