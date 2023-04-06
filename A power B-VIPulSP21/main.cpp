2283744b7632db0c33240b3fc91b78af157140d6
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-04-06 05:53:30 +0530

    Update main.cpp

--- A power B-VIPulSP21/main.cpp
+++ A power B-VIPulSP21/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
-main(){
+int main(){
   int num1,num2;
   cin>>num1;
   cin>>num2;
-  int square=num1**num2;
+  int square=pow(num1,num2);
   cout<<square;
 }
