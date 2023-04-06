adce7ac9f6fc6338d5e18994cd2c1ff64ca26b22
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-04-06 05:57:39 +0530

    Update main.cpp

--- Print larger number-VIPulSP21/main.cpp
+++ Print larger number-VIPulSP21/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main(){
   int num1,num2;
-  cin>>num1,num2;
+  cin>>num1;
+  cin>>num2;
   if (num1>num2){
-    return num1
+    cout<<num1;
   }
   else{
-    return num2
+    cout<<num2;
   }
 }
