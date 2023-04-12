3e925b9182bcc11998d934934b21d911a05eb136
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-04-12 18:30:07 +0530

    Create main.cpp

---
+++ Check if Pythagorean triplet or not-VIPulSP21/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int num1,num2,num3;
+  cin>>num1;
+  cin>>num2;
+  cin>>num3;
+  if (num1<0 || num2<0 || num3<0){
+    cout<<"NO"<<endl;
+  }
+  else if ((num1*num1)+(num2*num2)==num3*num3){
+    cout<<"YES"<<endl;
+  }
+  else if ((num1*num1)+(num3*num3)==num2*num2){
+    cout<<"YES"<<endl;
+  }
+  else if ((num3*num3)+(num2*num2)==num1*num1){
+    cout<<"YES"<<endl;  
+  }
+  else{
+    cout<<"NO"<<endl;
+  }
+}
