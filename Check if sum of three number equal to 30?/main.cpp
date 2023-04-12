4c65e63ea99810366f1f60a28f4539ccbd539287
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-04-12 18:52:37 +0530

    Create main.cpp

---
+++ Check if sum of three number equal to 30?/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int num1,num2,num3;
+  cin>>num1;
+  cin>>num2;
+  cin>>num3;
+  if (num1+num2+num3==20){
+    cout<<"YES"<<endl;
+  }
+  else{
+    cout<<"NO"<<endl;
+  }
+}
